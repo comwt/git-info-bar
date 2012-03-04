7bbc274f39ffb8aa2559d9519a774144932f5233
Justin Francis <jfrancis@inbox.com>
2012-03-04 02:44:16 -0600

    added GPL license and source info

--- source/bash-git/install.sh
+++ source/bash-git/install.sh
@@ -1,6 +1,26 @@
 #!/bin/bash
 
 #set -x
+################################################################################
+#
+# Copyright (C) 2012
+# by Justin Francis
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+#
+################################################################################
 
 cd $(dirname $0)
 ld_base=$PWD
@@ -48,7 +68,8 @@ Please run the following command to begin using bash-git:
 
     . ~/.bashrc
 
-Enjoy!
+Enjoy,
+ComWT (https://github.com/comwt/bash-git)
 
 "
 
